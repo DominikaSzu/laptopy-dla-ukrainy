e5c9466fde360328aee705a3aa1fc5c9ef34fbf4
DominikaSzu <dominika.szukala@gmail.com>
2022-03-08 21:44:19 +0100

    feat: add menu hover effects and button styled

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -21,6 +21,23 @@ p {
 ${tw`text-xl md:text-2xl`}
 }
 a {
-    ${tw`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-blue-700 rounded my-4 inline-block`}
+display: inline-block;
+background: #005bbb;
+  border: 2px solid;
+  font: inherit;
+  line-height: 1;
+  margin: 0.5em;
+  padding: 1em 2em;
+transition: 0.4s;
+color: #fff;
+&:hover,
+  &:focus { 
+    border-color: #ffd500;
+    color: #005bbb;
+  }
+  &:hover,
+&:focus {
+  box-shadow: inset 0 -3.25em 0 0 #ffd500;
+}
 }
 `;
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -19,11 +19,21 @@ export const Menu = () => {
         </StyledLogo>
 
         <StyledLinkList className="space-x-1">
-          <Link to={`/#${slugify('Jak mogę wziąć udział w inicjatywie?')}`}>chcę pomóc</Link>
-          <Link to={`/#${slugify('Potrzebuję pomocy')}`}>potrzebuję pomocy</Link>
-          <Link to={`/#${slugify('')}`}>o akcji</Link>
+          <StyledLink>
+            <Link to={`/#${slugify('Jak mogę wziąć udział w inicjatywie?')}`}>chcę pomóc</Link>
+          </StyledLink>
+          <StyledLink>
+            <Link to={`/#${slugify('Potrzebuję pomocy')}`}>potrzebuję pomocy</Link>
+            </StyledLink>
+          <StyledLink>
+            <Link to={`/#${slugify('')}`}>o akcji</Link>
+            </StyledLink>
+          <StyledLink>
           <Link to={`/#${slugify('Partnerzy')}`}>partnerzy</Link>
+          </StyledLink>
+          <StyledLink>
           <Link to={`/#${slugify('Kontakt')}`}>kontakt</Link>
+          </StyledLink>
         </StyledLinkList>
 
       <StyledMobileButton>
@@ -61,10 +71,33 @@ a {
 }`;
 
 const StyledLinkList = styled.div`
-${tw`hidden md:flex items-center`}
-a {
-  ${tw`py-5 px-3 text-gray-700 hover:text-gray-900`}
+${tw`hidden md:flex items-center`
 }`;
+
+const StyledLink = styled.div`
+display: inline-block;
+a{
+  ${tw`my-5 mx-3 text-gray-700 hover:text-gray-900`}
+  display: inline-block;
+  position: relative;
+	&::before {
+		content: "";
+		position: absolute;
+		width: 100%;
+		height: 3px;
+		bottom: 0;
+		margin: -5px 0;
+		background-color: #005bbb;
+		visibility: hidden;
+		transform: scaleX(0);
+		transition: all 0.4s ease-in-out 0s;
+	}
+	&:hover::before {
+		visibility: visible;
+		transform: scaleX(1);
+	}
+}
+`;
 
 const StyledMobileButton = styled.div`
 ${tw`md:hidden flex items-center`}
